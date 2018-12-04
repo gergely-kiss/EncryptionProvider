bc959258a979ab9101a697eebb5d4d5993565783
gergely <kiss-gergely@outlook.com>
2018-12-04 21:11:33 +0000

    init commit

---
+++ src/main/java/com/coding2d/EncryptionProvider/EncryptionProviderApplication.java
@@ -0,0 +1,12 @@
+package com.coding2d.EncryptionProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptionProviderApplication.class, args);
+	}
+}
